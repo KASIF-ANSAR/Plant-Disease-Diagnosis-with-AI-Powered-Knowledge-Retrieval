b11ebee219697cc21a98875213b42f094b8a7e29
Kasif Ansar <kasifansardudekula@gmail.com>
2025-11-09 16:15:41 +0530

    Removed node pipeline

--- flask/test.py
+++
@@ -1,4 +0,0 @@
-from google import genai
-client = genai.Client(api_key="AIzaSyCbZPjW9FHt9_mJ6I3K_iRlhAQnH6opaNo")
-resp = client.models.generate_content(model="gemini-2.5-flash", contents="Hello")
-print(resp.text)
